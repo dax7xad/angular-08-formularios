25ef65676b84108029dbe74d25b45ab98334bf57
Alvaro <dax7xad@gmail.com>
2018-09-03 21:12:14 -0600

    agregando validars property

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -12,11 +12,20 @@ export class DataComponent implements OnInit {
 
   constructor() {
     this.forma = new FormGroup({
-      'nombre': new FormControl('Fernando'),
-      'apellido': new FormControl(),
-      'correo': new FormControl()
+      'nombre': new FormControl('', [
+        Validators.required,
+        Validators.minLength(3)
+      ]),
+      'apellido': new FormControl('', [
+        Validators.required,
+        Validators.minLength(3)
+      ]),
+      'correo': new FormControl('', [
+        Validators.required,
+        Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')
+      ])
     });
-   }
+  }
 
   ngOnInit() {
   }

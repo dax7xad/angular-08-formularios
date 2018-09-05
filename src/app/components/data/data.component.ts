f875f6b8786ba90269f59009323feb04fc714449
Alvaro <dax7xad@gmail.com>
2018-09-04 21:21:19 -0600

    Ejercicio de controles hijos

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -7,31 +7,38 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styles: []
 })
 export class DataComponent implements OnInit {
-
   forma: FormGroup;
+
+  usuario: Object = {
+    nombrecompleto: {
+      nombre: 'Fernando',
+      apellido: 'herrera'
+    },
+    correo: 'Fernando.herrera85@gmail.com'
+  };
 
   constructor() {
     this.forma = new FormGroup({
-      'nombre': new FormControl('', [
+      nombrecompleto: new FormGroup({
+        nombre: new FormControl('', [
+          Validators.required,
+          Validators.minLength(3)
+        ]),
+        apellido: new FormControl('', [
+          Validators.required,
+          Validators.minLength(3)
+        ])
+      }),
+      correo: new FormControl('', [
         Validators.required,
-        Validators.minLength(3)
-      ]),
-      'apellido': new FormControl('', [
-        Validators.required,
-        Validators.minLength(3)
-      ]),
-      'correo': new FormControl('', [
-        Validators.required,
-        Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')
+        Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,3}$')
       ])
     });
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   guardarCambios() {
     console.log(this.forma);
   }
-
 }

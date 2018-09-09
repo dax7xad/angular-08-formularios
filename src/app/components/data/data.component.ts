edbd256f24036817f851d6babbc392c8ca099a6f
Alvaro <dax7xad@gmail.com>
2018-09-09 08:43:44 -0600

    seteando valores y restableciendo formulario

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -18,6 +18,7 @@ export class DataComponent implements OnInit {
   };
 
   constructor() {
+
     this.forma = new FormGroup({
       nombrecompleto: new FormGroup({
         nombre: new FormControl('', [
@@ -34,11 +35,26 @@ export class DataComponent implements OnInit {
         Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,3}$')
       ])
     });
+
+    // Cargar valores por defecto
+    this.forma.setValue(this.usuario);
+
+
   }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
   guardarCambios() {
     console.log(this.forma);
+    // restablecer el formulario (con valores por defecto)
+    // this.forma.reset(this.usuario);
+
+    // Restablecer el formulario (con valores vacios)
+    this.forma.reset({
+      nombrecompleto: {
+        nombre: '', apellido: ''
+      },
+      correo: '',
+    });
   }
 }
